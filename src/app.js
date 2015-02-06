dc522fa62c9b8c0b3e5831a7887eeddf873de476
Walter <wbrand@gmail.com>
2015-02-06 10:17:07 +0100

    sample with nested views

--- src/app.js
+++ src/app.js
@@ -1,2 +1,39 @@
 angular.module('feature',[]);
-angular.module('app',['feature']);
+angular.module('app',['feature', 'ui.router'], function($stateProvider){
+    var homeState = {
+        name:'home',
+        views: {
+            '':{
+                template:'<b>Home<ui-view name="featureQ"></ui-view></b>' // ui-view should become rb-view
+            },
+            'featureQ@home': {
+                template:'<div>my value is {{main.waarde}}</div>',
+                controller : 'featureQ.mainController',
+                controllerAs : 'main',
+                resolve : {
+                    app: function(){return {q:6}}
+                }
+            }
+        }
+    }
+    $stateProvider.state('home', homeState);
+
+    /* should become something like:
+        rbStateProvider.state('home', homeState)
+    */
+}).run(function($state){
+        $state.go('home');
+        /*
+            should become something like rbState.go('home')
+            the template in view '' should be the template that is passed to
+            layermanager.dispatch({
+                template : the emptystring view
+                controller: camodalcontroller,
+                data: the homestate object
+            })
+         */
+
+}).controller('featureQ.mainController', function(app){
+        var vm = this;
+        vm.waarde = app.q;
+    });
